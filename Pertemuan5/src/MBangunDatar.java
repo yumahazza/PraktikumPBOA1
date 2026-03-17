dba57b80d6291695eaeff152890a05599b3f93b0
yuma <yumatamahazza@gmail.com>
2026-03-17 13:51:40 +0700

    Add Class Bangundatar, Persegi, dan Lingkaran

---
+++ Pertemuan5/src/MBangunDatar.java
@@ -0,0 +1,27 @@
+// Nama file    : MBangunDatar.java
+// Deskripsi    : program aplikasi atribut dan method class BangunDatar
+// Pembuat      : Yuma Hazza Yuditama
+// Tanggal      : 17 Maret 2026
+
+public class MBangunDatar{
+    public static void main (String[] args){
+        Persegi Persegi1 = new Persegi(4, "biru", "hitam");
+        Lingkaran Lingkaran1 = new Lingkaran(14, "pink", "hitam");
+        BangunDatar Wow = new BangunDatar();
+        
+        Persegi1.printInfo();
+        System.out.println("");
+        Lingkaran1.printInfo();
+        System.out.println("");
+
+        System.out.println("Keliling Persegi1 : " + Persegi1.getKeliling());
+        System.out.println("Luas Persegi1 : " + Persegi1.getLuas());
+
+        System.out.println("Keliling Lingkaran1 : " + Lingkaran1.getKeliling());
+        System.out.println("Luas Lingkaran1 : " + Lingkaran1.getLuas());
+        System.out.println("");
+
+        Wow.printCounterBD();
+        
+    }
+}
