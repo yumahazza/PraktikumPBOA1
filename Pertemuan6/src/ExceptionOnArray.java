55cc2ac9f48212dd47e91f6c56a2c2cf275041ea
yuma <yumatamahazza@gmail.com>
2026-04-12 21:22:26 +0700

    Add ExceptionOnArray

---
+++ Pertemuan6/src/ExceptionOnArray.java
@@ -0,0 +1,22 @@
+/* Nama file    : ExceptionOnArray.java
+ * Deskripsi    : Program penggunaan eksepsi menggunakan class library Java
+ * Pembuat      : Yuma Hazza Yuditama
+ * Tanggal      : 12 April 2026
+*/
+
+public class ExceptionOnArray{
+    public static void main(String[] args){
+        // instansiasi object array integer
+        Integer[] arrayInteger = new Integer[4];
+        try{
+            arrayInteger[2] = 11;
+            arrayInteger[4] = 10;
+        } 
+        catch(ArrayIndexOutOfBoundsException exception){
+            exception.printStackTrace();
+        } 
+        finally{
+            System.out.println("clean up code...");
+        }
+    }
+}
