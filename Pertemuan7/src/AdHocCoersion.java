a6c9554f52147b487b820a71706f06f34b32c4da
yuma <yumatamahazza@gmail.com>
2026-04-23 22:42:42 +0700

    Coersion Done

--- Pertemuan7/src/AdHocCoersion.java
+++ Pertemuan7/src/AdHocCoersion.java
@@ -1,12 +1,17 @@
+/* Nama file    : AdHocCoercion.java
+ * Deskripsi    : Program untuk menerapkan konsep polimorfisme ad hoc coersion
+ * 
+ * */
+
 public class AdHocCoersion{
     public static void main(String[] args) throws Exception {
-        // bagian a
-        int x = 65;
+        /* bagian a */
+        Integer x = 65;
         String x1 = Character.toString(x);
-        double x2 = x;
+        Double x2 = x.doubleValue();
         String number = "999";
-        int number1 = Integer.parseInt(number);
-        double number2 = Double.parseDouble(number);
+        Integer number1 = Integer.parseInt(number);
+        Double number2 = Double.parseDouble(number);
 
         System.out.println("Bagian a");
         System.out.println("Nilai x: " + x);
@@ -16,13 +21,53 @@ public class AdHocCoersion{
         System.out.println("Nilai number1: " + number1);
         System.out.println("Nilai number2: " + number2);
         System.out.println();
-
-        // bagian b
-        int x2_int = (int) x2;
-
+        
+        /* bagian b */
+        Integer x2_int = x2.intValue();
+        
         System.out.println("Bagian b");
         System.out.println("Nilai x2 setelah diubah ke integer: " + x2_int);
+        System.out.println();
+        
+        /* bagian c */
+        String X = "1234";
+        String Y = "5678";
+        String S = X + Y;
+        Integer Z = Integer.parseInt(X) + Integer.parseInt(Y);
 
-        // bagian c
+        System.out.println("Bagian c");
+        System.out.println("Nilai S: " + S);
+        System.out.println("Nilai Z: " + Z);
+        System.out.println();
+        
+        /* bagian d */
+        String P = "12.34";
+        String Q = "56.78";
+        String R = P + Q;
+        Double D = Double.parseDouble(P) + Double.parseDouble(Q);
+        
+        System.out.println("Bagian d");
+        System.out.println("Nilai R: " + R);
+        System.out.println("Nilai D: " + D);
+        System.out.println();
+        
+        /*bagian e */
+        Integer A = Integer.parseInt(S);
+
+        System.out.println("Bagian e");
+        System.out.println("Nilai A: " + A);
+        System.out.println();
+        
+        /* bagian f */
+        // String T = Character.toString(A);
+        // eror, karena hasilnya terlalu besar untuk unicode
+
+        // perbaikan
+        String T = String.valueOf(A);
+        
+        System.out.println("Bagian f");
+        System.out.println("Nilai T: " + T);
+        System.out.println();
+
     }
 }
