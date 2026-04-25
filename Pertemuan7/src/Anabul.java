09c70eae4bc21cc8ed4ce15409eb8a4ac78d78ec
yuma <yumatamahazza@gmail.com>
2026-04-25 20:07:02 +0700

    Anabul oh Anabul

---
+++ Pertemuan7/src/Anabul.java
@@ -0,0 +1,36 @@
+/* Nama file    : Anabul.java
+ * Deskripsi    : Program untuk menerapkan konsep polimorfisme
+                universal inclusion pada class Anabul
+ * Pembuat      : Yuma Hazza Yuditama
+ * Tanggal      : 25 April 2026
+ * */
+
+public class Anabul{
+    /* ATRIBUT */
+    private String Nama;
+
+    /* METHOD / OPERATOR */
+    /* KONSTRUKTOR */
+    public Anabul(){
+        this.Nama = "kosong";
+    }
+
+    public Anabul(String P){
+        this.Nama = P;
+    }
+
+    /* SELEKTOR */
+    public String getNama(){
+        return Nama;
+    }
+
+    /* MUTATOR */
+    public void setNama(String P){
+        this.Nama = P;
+    }
+
+    /* METHOD LAINNYA */
+    public void printInfo(){
+        System.out.println("Nama    : " + getNama());
+    }
+}
