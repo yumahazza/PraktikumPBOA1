48bf802983362c9bab305fa7ec98ed25654733f0
yuma <yumatamahazza@gmail.com>
2026-05-17 22:23:57 +0700

    "Doneee"

---
+++ PersistentObjectDAO1/src/persistentobjectdao1/DAOManager.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package persistentobjectdao1;
+
+/**
+ * File : DAOManager.java
+ * Deskripsi : pengelola DAO dalam program
+ * @author yumah
+ */
+
+public class DAOManager {
+    private PersonDAO personDAO;
+
+    public void setPersonDAO(PersonDAO person) {
+        personDAO = person;
+    }
+
+    public PersonDAO getPersonDAO() {
+        return personDAO;
+    }
+}
